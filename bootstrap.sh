7c5b76ae6d41d952136a0811325ef0d062c3105c
Noelle Calliope <1581674+mekanoe@users.noreply.github.com>
2023-08-27 16:43:31 -0400

    bootstrap: pipefail to check for stupidness

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,13 @@
+set -o pipefail
+mount | grep -e '/mnt type' | cut -d' ' -f1 > /mnt/.remove-before-flight/rootfs
+set +o pipefail
+
 ucode=$(cat /proc/cpuinfo | grep -qe GenuineIntel > /dev/null && echo intel-ucode || echo amd-ucode)
 pacstrap -K /mnt linux-zen linux-zen-headers linux-firmware $ucode zsh git base-devel
 
 mkdir /mnt/.remove-before-flight > /dev/null 2>&1
 curl -sSL https://raw.githack.com/mekanoe/arch-setup/main/fresh.sh > /mnt/.remove-before-flight/fresh.sh
-mount | grep '/mnt type' | cut -d' ' -f1 > /mnt/.remove-before-flight/rootfs
+
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
