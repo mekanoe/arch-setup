234203d8a2f204cf1649142022561c9695fc693b
Noelle Calliope <1581674+mekanoe@users.noreply.github.com>
2023-08-27 16:34:13 -0400

    allow zsh plugins to be pre-pulled

--- setup.sh
+++ setup.sh
@@ -67,9 +67,13 @@ install_configs() {
   git clone https://github.com/mekanoe/arch-setup.git .
 
   # ZSH
+  rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  rm -rf ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
   git clone https://github.com/zsh-users/zsh-syntax-highlighting  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
+  rm -rf ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
   git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
   cp configs/.zshrc ~/.zshrc
 
   # Kitty
