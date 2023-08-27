66baf18b9cdbffd2b9f376d33d1ce000233db04f
Noelle Calliope <1581674+mekanoe@users.noreply.github.com>
2023-08-27 16:56:19 -0400

    bootstrap: call out early fail

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,12 @@
 set -e
 set -o pipefail
+echo ">> NOTE: If this fails early, you forgot to mount your partitions."
+mkdir /mnt/.remove-before-flight > /dev/null 2>&1
 mount | grep -e '/mnt type' | cut -d' ' -f1 > /mnt/.remove-before-flight/rootfs
 mount | grep -e '/mnt/boot type'
 set +o pipefail
 
+echo ">> Setting up mkinitcpio"
 mkdir -p /mnt/etc/mkinitcpio.d > /dev/null 2>&1
 mkdir -p /etc/mkinitcpio.d > /dev/null 2>&1
 cat <<EOF > /mnt/etc/mkinitcpio.d/linux-zen.preset
@@ -27,10 +30,11 @@ fallback_options="-S autodetect"
 EOF
 cp /mnt/etc/mkinitcpio.d/linux-zen.preset /etc/mkinitcpio.d/linux-zen.preset
 
+echo ">> Installing packages"
 ucode=$(cat /proc/cpuinfo | grep -qe GenuineIntel > /dev/null && echo intel-ucode || echo amd-ucode)
 pacstrap -K /mnt linux-zen linux-zen-headers linux-firmware $ucode zsh git base-devel
 
-mkdir /mnt/.remove-before-flight > /dev/null 2>&1
+echo ">> Pulling fresh.sh installer"
 curl -sSL https://raw.githack.com/mekanoe/arch-setup/main/fresh.sh > /mnt/.remove-before-flight/fresh.sh
 
 echo ">> Chrooting into new system. Run \`bash /.remove-before-flight/fresh.sh\` when you're ready."
