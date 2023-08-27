ba958db691489861bdf5a8a37614cd0919845bad
Noelle Calliope <1581674+mekanoe@users.noreply.github.com>
2023-08-27 16:47:45 -0400

    fresh: add echos

--- fresh.sh
+++ fresh.sh
@@ -5,15 +5,18 @@ set -e
 read -p "What's our hostname? =>> " new_hostname
 echo "$new_hostname" > /etc/hostname
 
+echo ">> Setting timezone"
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 
+echo ">> Setting hardware clock"
 hwclock --systohc
 
+echo ">> Setting locale"
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 
-ucode=$(cat /proc/cpuinfo | grep -qe GenuineIntel > /dev/null && echo intel-ucode || echo amd-ucode)
-
+echo ">> Installing systemd-boot"
+ucode=$(cat /proc/cpuinfo | grep -qe GenuineIntel > /dev/null && echo intel-ucode || echo amd-ucode) 
 bootctl install
 
 detected_root=$(cat /.remove-before-flight/rootfs)
@@ -35,7 +38,10 @@ initrd /initramfs-linux-zen.img
 options root=PARTUUID=$(blkid -s PARTUUID -o value $disk_dev) rw
 EOF
 
+echo ">> Generating fstab"
 genfstab -U / >> /etc/fstab
+
+echo ">> Setting up mkinitcpio"
 mkinitcpio -P
 
 echo ">> Setting root password"
@@ -46,9 +52,11 @@ echo ">> Creating user"
 useradd -m -G wheel -s /usr/bin/zsh noe
 passwd noe
 
+echo ">> Setting up sudo"
 tee /etc/sudoers.d/10-noe <<EOF
 noe ALL=(ALL) NOPASSWD:ALL
 EOF
 
+echo ">> Installing packages"
 curl -sSL https://raw.githack.com/mekanoe/arch-setup/main/setup.sh > /.remove-before-flight/setup.sh
 sudo -u noe bash /.remove-before-flight/setup.sh
